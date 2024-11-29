9748fcb76dbba9f86c7e07e095ef85047c341681
letiiciagoms <145374951+letiiciagoms@users.noreply.github.com>
2024-11-29 09:13:47 -0300

    v6

---
+++ Jogo1/Assets/Scripts/Grounded.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    Pato Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject.GetComponent <Pato> ();
+    }
+
+    void OnCollisionEnter2D (Collision2D collisor) 
+    {
+        if (collisor.gameObject.layer == 8) 
+        {
+            Player.isJumping = false;
+        }
+
+    }
+
+    void OnCollisionExit2D (Collision2D collisor)
+    {
+        if (collisor.gameObject.layer == 8) 
+        {
+            Player.isJumping = true;
+        }
+    }
+
+}
--- Jogo1/Assets/Scripts/Pato.cs
+++ Jogo1/Assets/Scripts/Pato.cs
@@ -6,6 +6,7 @@ public class Pato : MonoBehaviour
 {
     public float speed;
     public float JumpForce;
+    public bool isJumping;
 
     private Rigidbody2D rigidbody;
 
@@ -26,11 +27,23 @@ public class Pato : MonoBehaviour
     {
         Vector3 movement = new Vector3 (Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * speed;
+
+        float inputAxis = Input.GetAxis ("Horizontal");
+
+        if (inputAxis > 0)
+        {
+            transform.eulerAngles = new Vector2 (0f, 180f);
+        }
+
+         if (inputAxis < 0)
+        {
+            transform.eulerAngles = new Vector2 (0f, 0f);
+        }
     }
 
     void Jump () 
     {
-        if (Input.GetButtonDown ("Jump")) 
+        if (Input.GetButtonDown ("Jump") && !isJumping) 
         {
             rigidbody.AddForce (new Vector2 (0f, JumpForce), ForceMode2D.Impulse);
         }
