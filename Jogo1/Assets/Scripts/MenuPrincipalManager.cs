2f2fcb674207d1b52ac9f0e883b90919470d1a9c
letiiciagoms <145374951+letiiciagoms@users.noreply.github.com>
2024-12-10 11:27:21 -0300

    v11

---
+++ Jogo1/Assets/Scripts/MenuPrincipalManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipalManager : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDoJogo;
+
+    public void Jogar () 
+    {
+        SceneManager.LoadScene (nomeDoLevelDoJogo);
+    }
+
+    public void Sair ()
+    {
+        Debug.Log ("Sair do jogo");
+        Application.Quit ();
+    }
+
+
+}
